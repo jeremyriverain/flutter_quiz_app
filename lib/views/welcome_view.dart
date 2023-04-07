e4d76aa571fd9db4bc2845d2fa565a314f9e0c4c
Jérémy Riverain <jeremy@geekco.fr>
2023-04-07 08:52:55 +0200

    :lipstick: add launch icons

--- lib/views/welcome_view.dart
+++ lib/views/welcome_view.dart
@@ -8,79 +8,83 @@ class WelcomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 50.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ListTile(
-              title: Text(
-                "Let's play",
-                style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      color: const Color(0xFFF35B79),
-                      fontWeight: FontWeight.bold,
-                    ),
-              ),
-              subtitle: Text("Can you answer 6 questions in a row?",
-                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                        color: const Color(0xFFB3B0B0),
-                      )),
-            ),
-            const SizedBox(height: 60),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 15, vertical: 15.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(14),
-                  boxShadow: const [
-                    kBoxShadow,
-                  ],
-                  gradient: kBlueGradient,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              ListTile(
+                title: Text(
+                  "Let's play",
+                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        color: const Color(0xFFF35B79),
+                        fontWeight: FontWeight.bold,
+                      ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 30.0),
-                  child: Stack(
-                    clipBehavior: Clip.none,
-                    alignment: Alignment.bottomRight,
-                    children: [
-                      ListTile(
-                        onTap: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => QuestionView()),
-                          );
-                        },
-                        leading: const Icon(
-                          Icons.play_arrow_rounded,
-                          color: Colors.white,
-                          size: 35,
-                        ),
-                        title: Text(
-                          'Start a quiz',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headlineSmall
-                              ?.copyWith(
-                                fontWeight: FontWeight.bold,
-                              ),
-                        ),
-                      ),
-                      Positioned(
-                        top: -120,
-                        right: 40,
-                        child: Image.asset(
-                          'images/montgolfieres.png',
-                          width: 130,
-                        ),
-                      ),
+                subtitle: Text("Can you answer 6 questions in a row?",
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          color: const Color(0xFFB3B0B0),
+                        )),
+              ),
+              const SizedBox(height: 90),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 15.0),
+                child: Container(
+                  constraints: const BoxConstraints(maxWidth: 440),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(14),
+                    boxShadow: const [
+                      kBoxShadow,
                     ],
+                    gradient: kBlueGradient,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30.0),
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      alignment: Alignment.bottomRight,
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => QuestionView()),
+                            );
+                          },
+                          leading: const Icon(
+                            Icons.play_arrow_rounded,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                          title: Text(
+                            'Start a quiz',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headlineSmall
+                                ?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
+                        ),
+                        Positioned(
+                          top: -120,
+                          right: 40,
+                          child: Image.asset(
+                            'images/hot_air_balloons.png',
+                            width: 130,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
