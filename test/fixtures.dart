0013fc5809f06691d032a4265d144cf8da0f23eb
Jérémy Riverain <jeremy@geekco.fr>
2023-05-05 16:56:29 +0200

    :white_check_mark: add Confetti widget test

--- test/fixtures.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_quiz/model.dart';
-
-QuizEntry createQuizEntry() {
-  return QuizEntry(
-      question: 'to be or not to be ?',
-      correctAnswer: 'dream',
-      incorrectAnswers: ['not to be', 'to be']);
-}
